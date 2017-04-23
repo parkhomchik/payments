05df525db6641854589afac69afe7d3adf2dab81
aparkhomchik <parkhomchik@gmail.com>
2017-04-23 13:29:42 +0600

    add databse

---
+++ db/services.go
@@ -0,0 +1,9 @@
+package paymentdb
+
+import(
+	"github.com/parkhomchik/payments/models"
+)
+
+func GetServices(client payment.Client) (services []payment.Service, err error) {
+	return nil, new error("test")
+}
